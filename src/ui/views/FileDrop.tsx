59941a2808199ac0e595aeb4c74ed8bcf5c39b7a
Sanchit <raj.mb.coderx@gmail.com>
2020-09-09 22:27:15 +0530

    Initial functional version for Git Browser
    Initial functional version for Git Browser

---
+++ src/ui/views/FileDrop.tsx
@@ -0,0 +1,13 @@
+import * as React   from 'react';
+import { Container} from '@material-ui/core';
+
+/* TBD*/
+
+function FileDrop(){
+    return(
+        <Container>
+        </Container>
+    )
+}
+
+export default FileDrop;
